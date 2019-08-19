fd87ec78cbd6f302afd7459b3cd3f22d517422c0
Nguyễn Xuân Hoà <hoanx.neonstudio@gmail.com>
2019-08-19 16:53:34 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # rn-simple-toast
 React Native Toast component for both iOS and Android
+
+## Add it to your project
+
+1. Insall package
+    - Using NPM
+    `npm i -S rn-simple-toast` 
+    - Using Yarn
+    `yarn add rn-simple-toast`
+2. Import package
+    `import { Toast, DURATION, POSTION  } from 'rn-simple-toast';`
+
+## Usage
+
+
+
+```javascript
+import React from 'react';
+import { Toast, DURATION  } from 'rn-simple-toast';
+class App extends React.Component  {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Toast ref={_ref => this.toastRef = _ref} />
+                <Button
+                    onPress={() => {
+                        toastRef.show('Some message here', '#3498db', DURATION.LONG);
+                    }}
+                    title='Show message'
+                    style={{ width: 120, backgroundColor: 'darkviolet' }}
+                    textColor='#fff'
+                />
+            </View>
+        );
+    }
+}
+```
+
+```
+DEMO
+```
+![iOS](https://raw.githubusercontent.com/xuho/demo-images/master/rn-toast-ios.gif)
+![Android](https://raw.githubusercontent.com/xuho/demo-images/master/rn-toast-android.gif)
+
+
+## Props
+
+- **`duration`** _(String)_ - Duration time to display message.
+- **`containerStyle`** _(String)_ Style of toast container.
+- **`textStyle`** _(Function)_ - Style of text message
+- **`position`** _(String)_ - Message display position 
+- **`positionOffset`** _(String)_ - Distance from top or bottom to display message
+
+## Method
+- **`show`** - The method to show toast message
+
+**MIT Licensed**
---
+++ index.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { Text, Animated, Dimensions } from 'react-native';
+import PropTypes from 'prop-types';
+
+const { height, width } = Dimensions.get('window');
+const DURATION = {
+    LONG: 3000,
+    SHORT: 1500
+}
+const POSTION = {
+    BOTTOM: 'bottom',
+    TOP: 'top',
+    CENTER: 'center'
+}
+
+class Toast extends Component {
+    static defaultProps = {
+        duration: this.SHORT_DURATION,
+        position: POSTION.BOTTOM,
+        positionOffset: 64,
+        containerStyle: {
+            paddingVertical: 12, paddingHorizontal: 16, borderRadius: 8,
+            position: 'absolute', alignSelf: 'center', zIndex: 1000,
+            minWidth: 250, maxWidth: width - 16 * 2
+        },
+        textStyle: {
+            fontSize: 14, color: '#fff', textAlign: 'center'
+        }
+    }
+
+    state = {
+        isShow: false,
+        title: '',
+        color: ''
+    }
+
+    animatedOpacity = new Animated.Value(0);
+
+    show = (title, color = '#28a25d', duration = DURATION.SHORT) => {
+        this.setState({ isShow: true, title, color }, () => {
+            Animated.timing(this.animatedOpacity, {
+                toValue: 0.9,
+                duration: 500
+            }).start(() => {
+                setTimeout(() => {
+                    Animated.timing(this.animatedOpacity, {
+                        toValue: 0,
+                        duration: 500
+                    }).start(() => {
+                        this.setState({ isShow: false });
+                    });
+                }, duration);
+            });
+        })
+    }
+    get positionStyle() {
+        let { position, positionOffset } = this.props;
+        const { BOTTOM, TOP, CENTER } = POSTION;
+        switch (position) {
+            case TOP:
+                return {
+                    top: positionOffset
+                }
+            case CENTER:
+                return {
+                    top: height / 2
+                }
+            case BOTTOM:
+            default:
+                return {
+                    bottom: positionOffset
+                }
+        }
+    }
+    render() {
+        let { isShow, color, title } = this.state;
+        return isShow
+            ? <Animated.View style={[this.props.containerStyle, {
+                opacity: this.animatedOpacity,
+                backgroundColor: color
+            }, this.positionStyle]}>
+                <Text style={{ fontSize: 14, color: '#fff', textAlign: 'center' }}>
+                    {title}
+                </Text>
+            </Animated.View>
+            : null
+    }
+}
+
+Toast.propTypes = {
+    duration: PropTypes.number,
+    containerStyle: PropTypes.object,
+    textStyle: PropTypes.object,
+    position: PropTypes.string,
+    positionOffset: PropTypes.number
+}
+
+export {
+    Toast,
+    DURATION,
+    POSTION
+}
